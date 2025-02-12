ae8c3d6928feea21e3c6df506451932c2fc0535f
Ainars-ux <zaynlowskilL@gmail.com>
2025-02-12 12:55:22 +0200

    111

--- cr_db.py
+++ cr_db.py
@@ -2,12 +2,25 @@ import sqlite3
 
 db = sqlite3.connect('dati.db')
 sql = db.cursor()
-sql.execute("""CREATE TABLE rezultati (
-            id INTEGER NOT NULL UNIQUE,
+sql.execute("""CREATE TABLE IF NOT EXIST rezultati (
+            id INTEGER PRIMARY KEY AUTOINCRENT,
             vards TEXT,
-            klikski INTEGER,
-            laiks INTEGER,
-            datums TEXT,
-            PRIMARY KEY("id" AUTOINCREMENT)
+            klikski INTEGER NOT NULL,
+            laiks INTEGER NOT NULL,
+            datums TEXT NOT NULL,          
             )""")
+ieraksti = [
+    ('Anonīmais', 100, 100, "2020-01-01"),
+    ('Anonīmais', 100, 100, "2020-01-01"),
+    ('Anonīmais', 100, 100, "2020-01-01"),
+    ('Anonīmais', 100, 100, "2020-01-01"),
+    ('Anonīmais', 100, 100, "2020-01-01"),
+]
+sql.executemany('''
+INSERT INTO rezultati (vards, klikski, laiks, datums)
+                VALUES (?, ?, ?, ?)
+''', ieraksti)
+db.commit()
 db.close()
+
+print("Datubāze un ieraksti izveidoti!")
